27da91dd063f7009a593469a56e83a6c690bb9dc
Antismart <timbwamoses83@gmail.com>
2025-10-24 03:02:20 +0300

    Add src/config/pretium.js

---
+++ src/config/pretium.js
@@ -0,0 +1,3 @@
+export const PRETIUM_BASE_URL = process.env.PRETIUM_BASE_URL || 'https://api.pretium.co.ke';
+export const PRETIUM_API_KEY = process.env.PRETIUM_API_KEY || '';
+export const PRETIUM_HMAC_SECRET = process.env.PRETIUM_HMAC_SECRET || '';
